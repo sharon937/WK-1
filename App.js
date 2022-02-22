148ff40417e52d8aa10cdfbca19fe8b4b8183b11
sharon <sharonsnowman521@gmail.com>
2022-02-22 23:41:26 +0800

    hello

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>110919031</Text>
+      <Text>陳品璇:)</Text>
+      <StatusBar style="auto"/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
